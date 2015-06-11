7e8035133d6607baf8adc64d9cdd6076c8ea9bdd
purplyrocky <purplyrocky@gmail.com>
2015-06-11 08:51:58 -0700

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    concat : {
+      options: {
+        separator: '\n\n//------------------------------------------\n',
+        banner: '\n\n//------------------------------------------\n'
+      },
+      dist : {
+        src: ['components/scripts/*.js'],
+        dest: 'builds/development/js/script.js'
+      }
+    }, //concat
+
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files : [{
+          src: 'components/sass/style.scss',
+          dest: 'builds/development/css/style.css'
+        }]
+      }
+    }, //sass
+
+    watch: {
+      options: {
+        spawn: false,
+        livereload: true
+      },
+      scripts: {
+        files: ['builds/development/**/*.html',
+        'components/scripts/**/*.js',
+        'components/sass/**/*.scss'],
+        tasks: ['concat', 'sass']
+      }
+    }
+
+
+  }); //initConfig
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['concat', 'sass', 'watch']);
+
+}; //wrapper function
